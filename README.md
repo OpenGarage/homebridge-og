79d83061ad331b21acc66d79c24ff14edc2c2e62
Samer Albahra <salbahra@gmail.com>
2017-07-26 01:05:21 -0500

    Initial commit of updated plugin by @chickenmilk

---
+++ README.md
@@ -0,0 +1,33 @@
+# OpenGarage Garage door
+
+Homebridge plugin for [OpenGarage](https://opengarage.io).
+This plugin is a slighty modified version of [homebridge-loxone-garage](https://www.npmjs.com/package/homebridge-loxone-garage)
+made to work with OpenGarage
+
+# Installation
+
+1. Install homebridge using: npm install -g homebridge
+2. Install this plugin using: npm install -g homebridge-opengarage-v2
+3. Update your config.json configuration file. See the example below.
+
+# Example config.json:
+```
+	{
+		"accessories":
+		[
+			{
+				"accessory": "opengarage",
+				"name": "Garage",
+				"ip": "192.168.0.4",
+				"key": "YourPassword"
+			}
+		]
+	}
+```
+# NOTES
+
+1. Change the IP in the example to the IP or hostname of your OpenGarage.
+2. Be sure to change the "YourPassword" part to the password used for your OpenGarage.
+3. Enjoy telling Siri to open and close your Garage.
+4. You can also enable push notifications for your OpenGarage when it is opened or closed via the Home app on iOS now too.
+(Mine was enabled by default)
---
+++ index.js
@@ -0,0 +1,93 @@
+var request = require( "request" ),
+	fs = require( "fs" ),
+	Service, Characteristic;
+
+module.exports = function( homebridge ) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    homebridge.registerAccessory( "homebridge-opengarage", "opengarage", OpenGarageConnect );
+};
+
+function OpenGarageConnect( log, config ) {
+    this.log = log;
+
+    this.name = config.name;
+    this.ip = config.ip;
+    this.key = config.key;
+
+    this.garageservice = new Service.GarageDoorOpener( this.name );
+
+    this.garageservice
+        .getCharacteristic( Characteristic.CurrentDoorState )
+        .on( "get", this.getState.bind( this ) );
+
+    this.garageservice
+        .getCharacteristic( Characteristic.TargetDoorState )
+        .on( "get", this.getState.bind( this ) )
+        .on( "set", this.changeState.bind( this ) );
+
+    this.garageservice
+        .getCharacteristic( Characteristic.ObstructionDetected )
+        .on( "get", this.getStateObstruction.bind( this ) );
+};
+
+OpenGarageConnect.prototype.getStateObstruction = function( callback ) {
+    callback( null, false );
+};
+
+OpenGarageConnect.prototype.getState = function( callback ) {
+    this.log( "Getting current state..." );
+
+    request.get( {
+        url: "http://" + this.ip + "/jc"
+    }, function( err, response, body ) {
+        if ( !err && response.statusCode == 200 ) {
+            var json = JSON.parse( body ),
+				value = parseInt( json.door ),
+				state, statedoor;
+
+            if ( value === 0 ) {
+                state = "closed";
+                statedoor = true;
+            }
+
+            if ( value === 1 ) {
+                state = "open";
+                statedoor = false;
+            }
+
+            this.log( "Status garage: %s", state );
+            callback( null, statedoor );
+        } else {
+            this.log( "Error getting state: %s", err );
+            callback( err );
+        }
+    }.bind( this ) );
+};
+
+OpenGarageConnect.prototype.changeState = function( state, callback ) {
+    var doorState = ( state == Characteristic.TargetDoorState.CLOSED ) ? "closed" : "open";
+    this.log( "Set state to %s", doorState );
+
+    request.get( {
+        url: "http://" + this.ip + "/cc?dkey=" + this.key + "&click=1"
+    }, function( err, response, body ) {
+        if ( !err && response.statusCode === 200 ) {
+            this.log( "State change complete." );
+            var currentState = ( state == Characteristic.TargetDoorState.CLOSED ) ? Characteristic.CurrentDoorState.CLOSED : Characteristic.CurrentDoorState.OPEN;
+
+            this.garageservice
+                .setCharacteristic( Characteristic.CurrentDoorState, currentState );
+
+            callback( null );
+        } else {
+            this.log( "Error '%s' setting door state. Response: %s", err, body );
+            callback( err || new Error( "Error setting door state." ) );
+        }
+    }.bind( this ) );
+};
+
+OpenGarageConnect.prototype.getServices = function() {
+    return [ this.garageservice ];
+};
